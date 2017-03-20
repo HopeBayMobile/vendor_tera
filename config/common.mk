047d4737b47a24d12f9b74834217dcd913bb7ad4
canyugs <fineaisa@gmail.com>
2017-03-20 10:35:49 +0800

    Add vendor files for startup

---
+++ config/common.mk
@@ -0,0 +1,55 @@
+# Copyright (C) 2015 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+MY_LOCAL_PATH := vendor/tera
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-swap=false
+
+#BOARD_SEPOLICY_DIRS += \
+#    $(MY_LOCAL_PATH)/sepolicy
+#
+#BOARD_SEPOLICY_UNION += \
+#    hcfsapid.te \
+#    hcfsd.te \
+#    terafonn_app.te \
+#    file_contexts \
+#    fs_use \
+#    seapp_contexts \
+#    init_shell.te
+#
+
+PRODUCT_COPY_FILES += \
+    $(MY_LOCAL_PATH)/bin/hcfs:system/bin/hcfs:asus \
+    $(MY_LOCAL_PATH)/bin/HCFSvol:system/bin/HCFSvol:asus \
+    $(MY_LOCAL_PATH)/bin/hcfsapid:system/bin/hcfsapid:asus \
+    $(MY_LOCAL_PATH)/bin/hcfsconf:system/bin/hcfsconf:asus \
+    $(MY_LOCAL_PATH)/etc/hcfs.conf:system/etc/hcfs.conf:asus \
+    $(MY_LOCAL_PATH)/lib/libfuse.so:system/lib/libfuse.so:asus \
+    $(MY_LOCAL_PATH)/lib/liblz4.so:system/lib/liblz4.so:asus \
+    $(MY_LOCAL_PATH)/lib/libHCFS_api.so:system/lib/libHCFS_api.so:asus \
+    $(MY_LOCAL_PATH)/lib/libjansson.so:system/lib/libjansson.so:asus \
+    $(MY_LOCAL_PATH)/lib/libcurl.so:system/lib/libcurl.so:asus \
+    $(MY_LOCAL_PATH)/lib/libzip.so:system/lib/libzip.so:asus \
+    $(MY_LOCAL_PATH)/rootdir/init.hcfs.sh:root/init.hcfs.sh:asus \
+    $(MY_LOCAL_PATH)/rootdir/init.tera.rc:root/init.tera.rc:asus 
+
+#$(MY_LOCAL_PATH)/lib/libterafonnapi.so:system/lib/libterafonnapi.so:asus \
+
+## HCFS management app
+#PRODUCT_PACKAGES +=\
+#    HopebayHCFSmgmt \
+#    TeraService \
+#    Launcher3 \
+#    HBTUpdater
---
+++ rootdir/init.hcfs.sh
@@ -0,0 +1,96 @@
+#!/system/bin/sh
+
+MOUNTPT=/data
+HCFSSRC=/etc/hcfs.conf
+HCFSCONF=/data/hcfs.conf
+
+SMARTCACHE=/data/smartcache
+SMARTCACHEMTP=/data/mnt/hcfsblock
+LOOPDEV=/dev/block/loop6
+HCFSBLOCK=hcfsblock
+
+init_hcfs() {
+    rm -rf /data/data
+    rm -rf /data/app
+    mkdir /data/data
+    mkdir /data/hcfs
+    mkdir /data/hcfs/metastorage
+    mkdir /data/hcfs/blockstorage
+
+    /system/bin/hcfsconf enc ${HCFSSRC} ${HCFSCONF}
+
+    while [ ! -e ${HCFSCONF} ]; do sleep 0.1; done
+
+    /system/bin/hcfs -oallow_other,big_writes,subtype=hcfs,fsname=/dev/fuse &
+
+    while [ ! -e /dev/shm/hcfs_reporter ]; do sleep 0.1; done
+
+    #/system/bin/HCFSvol toggle_use_minimal_apk on 
+
+    /system/bin/HCFSvol create hcfs_data internal
+    /system/bin/HCFSvol mount hcfs_data /data/data
+
+    chown system:system /data/data
+    chmod 771 /data/data
+
+    mkdir /data/app
+    /system/bin/HCFSvol create hcfs_app internal
+    /system/bin/HCFSvol mount hcfs_app /data/app
+
+    chown system:system /data/app
+    chmod 771 /data/app
+
+    /system/bin/HCFSvol create hcfs_external external
+
+    mkdir /mnt/shell/emulated 0700 shell shell
+    mkdir /storage/emulated 0555 root root
+    mkdir /storage/emulated/legacy 0555 root root
+
+    /system/bin/HCFSvol mount hcfs_external /mnt/shell/emulated
+
+    mkdir /mnt/shell/emulated/0 0700 root sdcard_r
+    mkdir /mnt/shell/emulated/obb 0700 root sdcard_r
+    setprop init.svc.sdcard running 
+
+    #mkdir -p ${SMARTCACHEMTP}
+    #chmod 771 ${SMARTCACHEMTP}
+    #mkdir ${SMARTCACHE}
+    #/system/bin/HCFSvol create hcfs_smartcache internal
+    #/system/bin/HCFSvol mount hcfs_smartcache ${SMARTCACHE}
+    #chown system:system ${SMARTCACHE}
+    #chmod 771 ${SMARTCACHE}
+
+    #if [ ! -e ${SMARTCACHE}/${HCFSBLOCK} ]; then
+    #    #touch ${SMARTCACHE}/${HCFSBLOCK}
+    #    #truncate -s 100M ${SMARTCACHE}/${HCFSBLOCK}
+    #    dd if=/dev/zero of=${SMARTCACHE}/${HCFSBLOCK} bs=$((1024*1024)) count=100
+    #    losetup ${LOOPDEV} ${SMARTCACHE}/${HCFSBLOCK}
+    #    make_ext4fs ${LOOPDEV}
+    #else
+    #    losetup ${LOOPDEV} ${SMARTCACHE}/${HCFSBLOCK}
+    #fi
+    #mount -t ext4 ${LOOPDEV} ${SMARTCACHEMTP}
+
+    #chown system:system ${SMARTCACHEMTP}
+    #chmod 771 ${SMARTCACHEMTP}
+
+    #if [ ! -e /data/app/hcfsblock ]; then
+    #    touch /data/app/hcfsblock
+    #    truncate -s 4G /data/app/hcfsblock
+    #    losetup /dev/block/loop6 /data/app/hcfsblock
+    #    make_ext4fs /dev/block/loop6
+    #else
+    #    losetup /dev/block/loop6 /data/app/hcfsblock
+    #fi
+    #mount -t ext4 /dev/block/loop6 /data/data
+}
+
+################################################
+#
+# MAIN
+#
+################################################
+
+[ -n "`grep ${MOUNTPT} /proc/mounts | grep ext4`" ] && init_hcfs
+
+touch /tmp/hcfs_ready
